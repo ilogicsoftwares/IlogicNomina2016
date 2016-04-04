7ed89351a60cfc48059ae0ce6a8573f889192ca8
Richard Aguirre <richardjap@gmail.com>
2016-04-04 02:25:52 -0430

    Proyecto Agregado
    Primer Proyecto

---
+++ IlogicNomina2016/Viewmodels/LogingViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IlogicNomina2016.Helpers;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace IlogicNomina2016.Viewmodels
+{
+    class LogingViewModel:ViewModelBase
+    {
+        nominaEntities userContext= new nominaEntities();
+        public RelayCommand IngresarCommand { get;set; }
+        public LogingViewModel()
+        {
+            IngresarCommand = new RelayCommand(Ingresar);
+            CargarUsuarios();
+        }
+
+         void Ingresar(object parameter)
+        {
+            var passwordBox = parameter as PasswordBox;
+            var password = passwordBox.Password;
+            if (UsuarioActual != null && UsuarioActual.keyCi.Trim()== password.Trim())
+            {
+                MessageBox.Show("ingresando");
+            }
+            else
+            {
+                MessageBox.Show("Clave o Usuario Incorrecto");
+                passwordBox.Password = string.Empty;
+            }
+            
+
+        }
+
+        private void CargarUsuarios()
+        {
+            this.usuarios = userContext.users.ToList();
+        }
+
+        private List<users> _usuarios;
+        public List<users> usuarios
+        {
+            get
+            {
+                return _usuarios;
+            }
+            set
+            {
+                _usuarios = value;
+                RaisePropertyChanged("usuarios");
+                
+            }
+                        
+        }
+        private users _UsuarioActual;
+        public users UsuarioActual
+        {
+            get
+            {
+                return _UsuarioActual;
+            }
+            set
+            {
+                _UsuarioActual = (users)value;
+                
+                RaisePropertyChanged("UsuarioActual");
+            }
+        }
+
+        private string _UsuarioClave;
+        public string UsuarioClave
+        {
+            get{
+                return _UsuarioClave;
+            }
+            set {
+                _UsuarioClave = value;
+            }
+
+        }
+      
+
+
+    }
+}
---
+++ IlogicNomina2016/Viewmodels/ViewModelBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+using IlogicNomina2016.Helpers;
+
+namespace IlogicNomina2016.Viewmodels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        //basic ViewModelBase
+        public RelayCommand CommandCerrarWindow { get; set; }
+        public ViewModelBase()
+        {
+            CommandCerrarWindow = new RelayCommand(CerrarVentana);
+        }
+
+        private void CerrarVentana(object obj)
+        {
+            MainWindow ventana = obj as MainWindow;
+            ventana.Close();
+            
+        }
+
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Extra Stuff, shows why a base ViewModel is useful
+        bool? _CloseWindowFlag;
+        public bool? CloseWindowFlag
+        {
+            get { return _CloseWindowFlag; }
+            set
+            {
+                _CloseWindowFlag = value;
+                RaisePropertyChanged("CloseWindowFlag");
+            }
+        }
+
+        public virtual void CloseWindow(bool? result = true)
+        {
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+            {
+                CloseWindowFlag = CloseWindowFlag == null 
+                    ? true 
+                    : !CloseWindowFlag;
+            }));
+        }
+    }
+}
